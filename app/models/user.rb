8a381fd90c5f9f38ea0fea9ab75458684020cac6
James <JamesCarlson2@hotmail.com>
2015-10-27 10:09:29 -0500

    Install devise and generate devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_save { self.role ||= :standard }
+
+  enum role: [:standard, :premium, :admin]
+
+  def self.avatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+end
